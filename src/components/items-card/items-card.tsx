6534c67a6d573e06542866c6d7308fc519e55085
Gustavo Penalver <gupramallo@gmail.com>
2025-08-10 21:07:23 +0200

    small fix 2

--- src/components/items-card/items-card.tsx
+++ src/components/items-card/items-card.tsx
@@ -1,8 +1,8 @@
 import AddButton from '../atoms/buttons/add-button'
 import DeleteButton from '../atoms/buttons/delete-button'
 import RevertButton from '../atoms/buttons/revert-button'
-import CardWithActionButtons from '../card-with-action-buttons'
 import ItemsBox from '../items-box'
+import { Card } from './styles'
 import type { ItemsCardProps } from './types'
 
 const ItemsCard: React.FC<ItemsCardProps> = ({
@@ -13,7 +13,7 @@ const ItemsCard: React.FC<ItemsCardProps> = ({
   deleteSelectedItems,
   revertLastAction,
 }) => (
-  <CardWithActionButtons
+  <Card
     centeredText
     title="This is a technical proof."
     description="Lorem ipsum dolor sit amet consectetur adipiscing, elit mus primis nec inceptos. Lacinia habitasse arcu molestie maecenas cursus quam nunc, hendrerit posuere augue fames dictumst placerat porttitor, dis"
@@ -26,7 +26,7 @@ const ItemsCard: React.FC<ItemsCardProps> = ({
     rightButtons={<AddButton variant="outlined" onClick={openDialog} />}
   >
     <ItemsBox items={items} onClick={toggleItem} onDoubleClick={deleteItem} />
-  </CardWithActionButtons>
+  </Card>
 )
 
 export default ItemsCard
---
+++ src/components/items-card/styles.ts
@@ -0,0 +1,6 @@
+import { styled } from '@mui/material'
+import CardWithActionButtons from '../card-with-action-buttons'
+
+export const Card = styled(CardWithActionButtons)`
+  align-self: center;
+`
