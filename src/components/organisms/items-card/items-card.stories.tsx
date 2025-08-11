bd71de75b3d19892754a6555b6c9e6bf2ce4e235
Gustavo Penalver <gupramallo@gmail.com>
2025-08-11 02:12:50 +0200

    items card story

---
+++ src/components/organisms/items-card/items-card.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+import { mockItems } from '../../../shared/items-provider/utils/__mocks__/item-provider-utils-mocks'
+import Layout from '../../atoms/layout'
+import ItemsCard from './items-card'
+
+const meta: Meta = {
+  title: 'Organisms/Items Card',
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export default meta
+
+export const AddItemDialogStory: StoryObj<{
+  numberOfItems: number
+  open: boolean
+}> = {
+  render: () => (
+    <Layout>
+      <ItemsCard
+        items={mockItems}
+        toggleItem={() => {}}
+        deleteItem={() => {}}
+        deleteSelectedItems={() => {}}
+        revertLastAction={() => {}}
+        openDialog={() => {}}
+      />
+    </Layout>
+  ),
+}
